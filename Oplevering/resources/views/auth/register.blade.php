8483945ca58cf612c0319a4a79bb94a9603abdbe
Stefan Teunissen <stefanteunissen1@gmail.com>
2019-04-18 01:16:07 +0200

    Niks

--- Oplevering/resources/views/auth/register.blade.php
+++ Oplevering/resources/views/auth/register.blade.php
@@ -261,7 +261,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Registreren') }}
+                                    Registreren
                                 </button>
                             </div>
                         </div>
