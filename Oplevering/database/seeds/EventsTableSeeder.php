84554ebb63135ab7fc9beef29499211dbb88be36
00271268@rijnijssel.nl <00271268@rijnijssel.nl>
2019-04-17 13:32:42 +0100

    basic events

--- Oplevering/database/seeds/EventsTableSeeder.php
+++ Oplevering/database/seeds/EventsTableSeeder.php
@@ -25,5 +25,20 @@ class EventsTableSeeder extends Seeder
             'finished' => '0',
             'place_points' => '66/666, 1',
         ]);
+
+        DB::table('go_vadis_events')->insert([
+            'name' => 'Test Event2',
+            'description' => 'Test Event Description2',
+            'orginazor' => '6662',
+            'category' => 'Test Category(NTF)2',
+            'date' => '02-03-2000(NTF)2',
+            'postal_number' => '6666 NTF2',
+            'house_number' => '6662',
+            'street' => 'NeedToFix street2',
+            'signed_up' => '6662',
+            'max_signed_up' => '66662',
+            'finished' => '1',
+            'place_points' => '66/666, 12',
+        ]);
     }
 }
