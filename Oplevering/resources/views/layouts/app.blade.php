c52b9a95133ccb23d5b8236fc1f88a2777a81479
Levi Hentzepeter <00283682@rijnijssel.nl>
2019-04-17 15:39:04 +0200

    title

--- Oplevering/resources/views/layouts/app.blade.php
+++ Oplevering/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Govadis</title>
 
     <!-- Scripts -->
     <script src="{{asset('js/jquery-3.4.0.min.js')}}"></script>
